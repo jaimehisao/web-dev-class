bf9f6d679876631b7cf6ae40a1149fa83c54e965
Hisao <jaimehisao2011@gmail.com>
2021-10-24 15:48:45 +0200

    added auth router modifications

--- routers/auth-router.js
+++ routers/auth-router.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const bcrypt = require('bcrypt');
 
 const ADMIN_USERNAME = '1234'
-const ADMIN_PASSWORD = '1234'
+const ADMIN_PASSWORD = '$2a$10$uwmh5uFZArJXWb2/5./J2eNYDYWoe7Rx/phl0u7mEQQkmF0SEcB.m'
 
 const router = express.Router()
 
@@ -14,8 +15,7 @@ router.post('/login', function(request, response){
     const username = request.body.username
     const password = request.body.password
     //console.log(request)
-
-    if(username === ADMIN_USERNAME && password === ADMIN_PASSWORD){
+    if(username === ADMIN_USERNAME && bcrypt.compareSync(password, ADMIN_PASSWORD)){
         request.session.isLoggedIn = true
 
         //In the case that the user logs in.
@@ -24,9 +24,22 @@ router.post('/login', function(request, response){
         response.redirect('/')
     }else{
         // TODO: Display error message to the user.
-        response.render('login.hbs')
+
+        const model = {
+            errors: ['Failed to log in! Try again with other credentials']
+        }
+        response.render('login.hbs', model)
     }
 
+})
+
+router.get('/logout', function(request, response){
+    response.render('logout.hbs')
+})
+
+router.post('/logout', function(request, response){
+    request.session.isLoggedIn = false
+    response.redirect('/')
 })
 
 module.exports = router
